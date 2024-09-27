90d65462cac1e989ca3c72f89e29bfc8fa159245
Joshua Clark <Joshrclark02@hotmail.com>
2024-09-27 12:29:37 -0500

    Mini Project (a2): Bubble Cursor Experiment
    Everything should be setup right, I hope lol

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const grid_window_bounds = grid_window.getBoundingClientRect();
 var color_unclicked = 'rgb(221, 182, 224)'
 var color_clicked = 'rgb(108, 148, 111)'
 
+let current_target = null;
+
 
 let dot_radius;
 let min_x;
@@ -73,14 +75,30 @@ function create_grid(){
     
     refresh_button.setAttribute('fill', '#FF7F3E')
 }
+
+let last_flip_time = Date.now()
 function flipButton(r, c){
     let dot_id = 'dot-'+ r + '_' + c
     console.log("trying to flip " + dot_id)
     let dot = document.getElementById(dot_id);
-    if(color_unclicked === dot.style.backgroundColor){
-        dot.style.backgroundColor = color_clicked
-    }else{
-        dot.style.backgroundColor = color_unclicked
+    if(!test_mode || dot.classList.contains('current-target')){
+        if(color_unclicked === dot.style.backgroundColor){
+            dot.style.backgroundColor = color_clicked
+        }else{
+            dot.style.backgroundColor = color_unclicked
+        }
+    }
+
+    // if we are in test mode log all of their clicks even misclicks
+    if(test_mode){
+        let curr_flip_time = Date.now()
+        let dot_id_num = r * cols_picker.value + c
+        test_data.push([uuid, current_mode ? 'bubble' : 'normal', dot_id_num, curr_flip_time - last_flip_time])
+        
+        if(dot.classList.contains('current-target')){
+            last_flip_time = curr_flip_time
+            highlight_next()
+        }
     }
 }
 
@@ -99,21 +117,26 @@ dot_size_picker.onchange = function() {
 }
 
 // adding tracking of bubble mode
-var bubble_mode_picker = document.getElementById('bubble-enabled')
-bubble_mode_picker.onchange = function(){
-    console.log(bubble_mode_picker.checked)
-    // if(bubble_mode_picker.checked){
-    //     bubble_mode_picker.text = '✔'
-    // }else{
-    //     bubble_mode_picker.text = '✘'
-    // }
 
-    // TODO: make this so it actually enables or disables the overlay
+var bubble_mode_picker = document.getElementById('bubble-enabled')
+let current_mode = bubble_mode_picker.checked
+
+var method_explanation_dom = document.getElementById('method-explanation')
+const normal_explanation = 'Normal Mode! You will select the dot directly under your cursor.'
+const bubble_explanation = 'Bubble Mode! You will select the closest dot. This area is highlighted by the shadow around your cursor.'
+method_explanation_dom.textContent = normal_explanation
+
+
+bubble_mode_picker.onchange = function(){
+    current_mode = bubble_mode_picker.checked
+    console.log(bubble_mode_picker.checked)
+
+    method_explanation_dom.textContent = bubble_mode_picker.checked ? bubble_explanation : normal_explanation;
 }
 
 let close_dot;
 onmousemove = function(e){
-    if(bubble_mode_picker.checked){    
+    if(current_mode){    
         // console.log("mouse location:", e.clientX, e.clientY)
 
         // finding which is the closest dot
@@ -173,6 +196,10 @@ onmousemove = function(e){
 
 onclick = function(e){
     console.log(e);
+    // dont click through when the modal is displayed
+    if(modal.style.display == 'flex'){
+        return;
+    }
     // are we in bubble mode, is the click in the right area, are we double clicking?
     if(bubble_mode_picker.checked && e.clientY >= grid_window_bounds.top && !e.target.classList.contains('dot')){
         // we are clicking on the grid window click the nearest button
@@ -190,4 +217,79 @@ function display_grid(){
             
         }
     }
+}
+
+
+let test_mode = false;
+let modal_dom = document.getElementById('modal')
+let header_dom = document.getElementById('header')
+let test_data = [[]]
+let uuid = crypto.randomUUID();
+
+function setup_test(){
+    test_mode = true;
+
+    rows_picker.value = 4;
+    cols_picker.value = 5;
+    header_dom.style.setProperty('display', 'none')
+    create_grid()
+    modal_dom.style.setProperty('display', 'flex')
+    
+    test_data = [['participant ID', 'cursor type', 'target #', 'selection time']]
+}
+
+let order = []
+function start_test(event){
+    event.stopPropagation();
+
+    create_grid()
+    // generate the order of them to be highlighted
+    order = Array(rows_picker.value * cols_picker.value).fill().map((_, idx) => idx)
+    order = order.sort(() => Math.random() - .5)
+    console.log(order)
+    
+    // highlight the first one
+    highlight_next()
+
+    // hide the modal
+    modal_dom.style.setProperty('display', 'none')
+
+    // set the epoch time
+    last_flip_time = Date.now()
+}
+
+function highlight_next() {
+    if(current_target){
+        current_target.classList.remove('current-target')
+    }
+
+    if(order.length == 0){
+        end_test()
+        return
+    }
+    let next_id_num = order.shift()
+    let next_id = 'dot-' + Math.floor(next_id_num / cols_picker.value) + '_' + (next_id_num % cols_picker.value);
+    console.log(next_id)
+    current_target = document.getElementById(next_id)
+    current_target.classList.add('current-target')
+}
+
+function end_test(){
+    modal_dom.style.setProperty('display', 'flex')
+    current_target = null
+    bubble_mode_picker.click()
+}
+
+// https://www.geeksforgeeks.org/how-to-create-and-download-csv-file-in-javascript/
+function export_data(){
+    let csv_formatted_data = test_data.map(row => row.join(',')).join('\n')
+
+    const blob = new Blob([csv_formatted_data], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    
+    const a = document.createElement('a');
+    
+    a.href = url;
+    a.download = 'results-' + uuid + '.csv';
+    a.click();
 }
